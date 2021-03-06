78dd753cbc46892eb5a174a10dc2f1e23ccbe73d
Patrick <patrick.starke@yahoo.de>
2021-03-06 18:20:06 +0100

    Update environ variables

--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,5 @@
-import logging
 from __future__ import with_statement
+import logging
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
--- models.py
+++ models.py
@@ -18,7 +18,7 @@ DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
 
 # Heroku DB
 # DB_PATH = os.environ('DB_PATH')
-DB_PATH = 'postgres://rtmfcdettevdpc:1ab7e5292d255864ac4a0c67193aea7f994a0a5f8c16de26086b49c04206b0a4@ec2-50-19-176-236.compute-1.amazonaws.com:5432/df935uv7s2p4i8'
+DB_PATH = os.environ.get('DB_PATH')
 
 db = SQLAlchemy()
 
